620a14e754369264bee453fff46b40529511f57a
viragbalazs <viragbalazs95@gmail.com>
2018-10-12 11:53:10 +0200

    nmbrintext

---
+++ gyak.js
@@ -0,0 +1,14 @@
+const numberInTex = {
+  1: 'egy',
+  2: 'kettő',
+  3: 'három',
+  4: 'négy',
+  5: 'öt',
+  6: 'hat',
+  7: 'hét',
+  8: 'nyolc',
+  9: 'kilenc',
+  10: 'tíz'
+};
+
+module.exports(numberInTex);
